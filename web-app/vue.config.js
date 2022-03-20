3ed7243fe490975295e3970d12e1f1fa4806a714
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2022-03-20 22:57:14 +0000

    Added title to vue.config.js

---
+++ web-app/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    filenameHashing: false,
+    pages: {
+        "index": {
+            entry: 'src/main.js',
+            title: "Diabetic unit's manager"
+        }
+    }
+};
