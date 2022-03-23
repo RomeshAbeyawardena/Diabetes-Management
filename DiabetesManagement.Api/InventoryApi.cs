9dd34ba3ddc250b752e7e8f53e0f2083e5e01e8d
romesh-abeyawardena <romesh.abeyawardena@concerto.co.uk>
2022-03-23 08:58:00 +0000

    Added exception handlers

--- DiabetesManagement.Api/InventoryApi.cs
+++ DiabetesManagement.Api/InventoryApi.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -20,6 +21,12 @@ namespace DiabetesManagement.Api
     {
         private readonly ILogger<InventoryApi> _logger;
         private const string ConnectionString = "Server=dotnetinsights.database.windows.net;Initial Catalog=DiabetesUnitsManager;User Id=romesh.a;password=e138llRA1787!;MultipleActiveResultSets=true";
+
+        private static IActionResult HandleException(Exception exception)
+        {
+            return new BadRequestObjectResult(exception.Message);
+        }
+
         public InventoryApi(ILogger<InventoryApi> log)
         {
             _logger = log;
@@ -34,9 +41,7 @@ namespace DiabetesManagement.Api
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(InventoryHistory), Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest request)
-        {
-            int? versionNumber = null;
-
+        {            
             var requiredConditions = new[]
             {
                 request.Query.TryGetValue("key", out var key),
@@ -45,6 +50,8 @@ namespace DiabetesManagement.Api
 
             if(requiredConditions.All(a => a) && Guid.TryParse(userIdValue, out var userId))
             {
+                int? versionNumber = null;
+
                 if (request.Query.TryGetValue("version", out var version) && int.TryParse(version, out int versionNum))
                 {
                     versionNumber = versionNum;
@@ -70,22 +77,40 @@ namespace DiabetesManagement.Api
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         public async Task<IActionResult> Save([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request)
         {
-            using var postHandler = new Post(ConnectionString);
+            try
+            {
+                using var postHandler = new Post(ConnectionString);
 
-            var requiredConditions = new[] { request.Form.TryGetValue("items", out var items),
+                var requiredConditions = new[] { request.Form.TryGetValue("items", out var items),
                 request.Form.TryGetValue("key", out var key),
                 request.Form.TryGetValue("userId", out var userIdValue) };
-           
-            if(requiredConditions.All(a => a) && Guid.TryParse(userIdValue, out var userId))
-            {
-                await postHandler.Save(new InventoryHistory { Items = items, Key = key, UserId = userId });
+
+                if (requiredConditions.All(a => a))
+                {
+                    if (Guid.TryParse(userIdValue, out var userId))
+                    {
+                        await postHandler.Save(new InventoryHistory { Items = items, Key = key, UserId = userId });
+                    }
+                    else
+                        throw new InvalidOperationException("User id is in an invalid format");
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Expected form values: items, key, userId. " +
+                        $"Received: { (requiredConditions[0] ? "items" : "") }," +
+                        $" { (requiredConditions[0] ? "key" : "") }, " +
+                        $" { (requiredConditions[0] ? "userId" : "") }");
+                }
+                return new OkObjectResult(items);
             }
-            else
+            catch (InvalidOperationException invalidOperationException)
             {
-                return new BadRequestResult();
+                return HandleException(invalidOperationException);
             }
-            
-            return new OkObjectResult(items);
+            catch (DataException dataException)
+            {
+                return HandleException(dataException);
+            }
         }
     }
 }
