5fde5b4c45f400d48da7d5a93c9535b081b80866
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2022-04-02 00:41:20 +0100

    getting my head around Vue3

--- v3/web-app/src/App.vue
+++ v3/web-app/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Dialog from 'primevue/dialog';
+import { ref } from 'vue'
+import { useStore } from './stores';
+
+const store = ref(useStore());
+
 </script>
 
 <template>
+  <Dialog />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
