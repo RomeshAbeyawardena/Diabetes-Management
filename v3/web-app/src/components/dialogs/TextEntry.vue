f55a276b034487f33b463b1eacca9a676cdc6ecb
romesh-abeyawardena <romesh.abeyawardena@concerto.co.uk>
2022-04-12 16:34:13 +0100

    Updated vues

--- v3/web-app/src/components/dialogs/TextEntry.vue
+++ v3/web-app/src/components/dialogs/TextEntry.vue
@@ -1,14 +1,13 @@
 <script setup>
-    import { ref, watch } from 'vue';
-    import Textarea from 'primevue/textarea';
+import Textarea from 'primevue/textarea';
+import { ref, watch } from 'vue';
+const props = defineProps({ value: String });
+const emit = defineEmits(['value:updated']);
+const value = ref(props.value);
 
-    const props = defineProps({ value: String });
-    const emit = defineEmits(['value:updated']);
-    const value = ref(props.value);
-
-    watch(value, (newValue) => {
-        emit("value:updated", newValue);
-    });
+watch(value, (newValue) => {
+    emit("value:updated", newValue);
+});
 </script>
 <template>
     <Textarea style="width:100%; margin-bottom:1rem" v-model="value" :autoResize="false" rows="8" cols="30" />
---
+++ v3/web-app/src/components/dialogs/VersionPicker.vue
@@ -0,0 +1,8 @@
+<script setup>
+import MultiSelect from 'primevue/multiselect';
+const selectedInventory = ref(null);
+const inventories = ref([]);
+</script>
+<template>
+    <MultiSelect v-model="selectedInventory" :options="inventories" optionLabel="version" placeholder="Select version" />
+</template>
