c2dbc3ce571d93d47f0bbf3d2bd45b0b1ff4a1aa
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2022-04-12 22:54:33 +0100

    Added version styles

--- v3/web-app/src/components/dialogs/VersionPicker.vue
+++ v3/web-app/src/components/dialogs/VersionPicker.vue
@@ -1,6 +1,6 @@
 <script setup>
-
-import Dropdown from 'primevue/dropdown';
+import "../../scss/version-picker.scss"
+import Dropdown from "primevue/dropdown";
 import { onBeforeMount } from "vue-demi";
 import { useInventoryStore } from "../../stores/Inventory";
 import { ref } from "vue";
@@ -12,26 +12,31 @@ const inventoryVersions = ref([]);
 onBeforeMount(async () => {
   const versions = await store.loadVersions();
   inventoryVersions.value = versions;
-  
 });
 </script>
 <template>
-  <div class="field">
-    <label style="margin-right: 0.5rem;">Version:</label>
-    <Dropdown
-      :filter="true"
-      :showClear="true"
-      v-model="selectedInventory"
-      :options="inventoryVersions"
-      optionLabel="version"
-      placeholder="Select version"
-    >
-     <template #value="slotProps">
-       <div>Version {{slotProps.value}}</div>
-     </template>
-     <template #option="slotProps">
-       <div>Version {{slotProps.option.version}}</div>
-     </template>
-    </Dropdown>
+  <div class="version-picker">
+    <div class="field grid">
+      <div class="col-2">
+        <label style="margin-right: 0.5rem">Version:</label>
+      </div>
+      <div class="col-6">
+        <Dropdown
+          :filter="true"
+          :showClear="true"
+          v-model="selectedInventory"
+          :options="inventoryVersions"
+          optionLabel="version"
+          placeholder="Select version"
+        >
+          <template #value="slotProps">
+            <div>Version {{ slotProps.value }}</div>
+          </template>
+          <template #option="slotProps">
+            <div>Version {{ slotProps.option.version }}</div>
+          </template>
+        </Dropdown>
+      </div>
+    </div>
   </div>
 </template>
