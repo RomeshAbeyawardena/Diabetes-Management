fc1285249ed3a700459246c277f678d942406aa6
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2022-04-23 17:16:48 +0100

    Added validation

--- DiabetesManagement.Api/v3/Inventory/Features/User/GetRequest.cs
+++ DiabetesManagement.Api/v3/Inventory/Features/User/GetRequest.cs
@@ -1,12 +1,16 @@
 ﻿using Inventory.Attributes;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace Inventory.Features.User;
 [RequiresClaims(Permissions.User_View)]
 public class GetRequest : IRequest<Models.User>
 {
     public Guid? UserId { get; set; }
+    [Required(ErrorMessage = "Email address is required")]
     public string? EmailAddress { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")]
     public string? Password { get; set; }
     public bool AuthenticateUser { get; set; }
 }
